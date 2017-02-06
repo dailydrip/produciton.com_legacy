27018cde312d4cf0e228c3fc2a8903c617af7d15
Matt <matt24ray@gmail.com>
2017-02-06 12:05:23 -0600

    Josh Magic

--- my-app/src/App.elm
+++ my-app/src/App.elm
@@ -5,31 +5,114 @@ import Html.Attributes exposing (src)
 import Html.Events exposing (..)
 
 
+-- Model - the data that your app needs in order to work,
+-- esp. data that can change.
+
+
 type alias Model =
-    { message : String
-    , logo : String
+    { templates : List Template
+    , todoList : Maybe TodoList
     }
+
+
+type alias TodoList =
+    { todos : List Todo
+    , name : String
+    , id : String
+    }
+
+
+type alias Todo =
+    { title : String
+    , completed : Bool
+    }
+
+
+type alias Template =
+    -- This is a record
+    { name : String
+    , logoUrl : String
+    , id : String
+    }
+
+
+
+-- first argument is 'flags' that are sent in when starting
+-- the app from js
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+    { templates = initialTemplates
+    , todoList = Nothing
+    }
+        ! []
+
+
+initialTemplates : List Template
+initialTemplates =
+    [ { name = "Rails"
+      , logoUrl = ""
+      , id = "0"
+      }
+    , { name = "Ember"
+      , logoUrl = ""
+      , id = "0"
+      }
+    ]
+
+
+fakeTodoList : TodoList
+fakeTodoList =
+    { todos = []
+    , name = "Josh's fancy todo list"
+    , id = "foo"
+    }
 
 
 type Msg
     = NoOp
+    | CreateTodoListFromTemplate String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        CreateTodoListFromTemplate templateId ->
+            { model | todoList = Just fakeTodoList } ! []
 
 
 view : Model -> Html Msg
 view model =
+    case model.todoList of
+        Nothing ->
+            viewTemplates model
+
+        Just todoList ->
+            viewTodoList todoList
+
+
+viewTodoList : TodoList -> Html Msg
+viewTodoList todoList =
+    h2 [] [ text "todolist" ]
+
+
+viewTemplates : Model -> Html Msg
+viewTemplates model =
     div []
-        [ button [ text "Stuff"]
-        ]
+        ([ h2 [] [ text "Produciton" ] ]
+            ++ (List.map templateButton model.templates)
+        )
+
+
+templateButton : Template -> Html Msg
+templateButton template =
+    button
+        [ onClick (CreateTodoListFromTemplate template.id) ]
+        [ text template.name ]
 
 
 subscriptions : Model -> Sub Msg
